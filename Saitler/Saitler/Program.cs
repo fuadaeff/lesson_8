0bc64e6cb0449cf89b5e12db7529faa3914e36cf
fuadaeff <fabdulla057@gmail.com>
2025-02-14 18:44:34 +0400

    Add files via upload

---
+++ Saitler/Saitler/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Saitler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Saitler("salam,necesiniz?"));
+        }
+        static int Saitler(string text)
+        {
+            string saitler = "aeiouAEİOU";
+            int count = 0;
+
+            foreach (char c in text)
+            {
+                if (saitler.Contains(c))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
